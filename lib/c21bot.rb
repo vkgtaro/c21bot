4ec2183d3f1908d78cb92f2b49d49f5006838c6d
Daisuke Komatsu <komatsu@cb21.co.jp>
2010-06-11 19:14:14 +0900

    added serif, config and controller

--- lib/c21bot.rb
+++ lib/c21bot.rb
@@ -6,6 +6,8 @@ require 'rubygems'
 require 'twitter'
 require 'logger'
 require 'oauth'
+require 'yaml'
+require 'singleton'
 require 'c21bot/oauth-patch'
 
 require 'pp'
@@ -45,8 +47,7 @@ module C21
       end
 
       def auto_refollow
-        thanks_message = @thanks_for_follow ? @thanks_for_follow
-                                            : "フォローありがとうございます！"
+        thanks_message = @thanks_for_follow ? @thanks_for_follow : "フォローありがとうございます！"
         friends = @twitter.friends
         @twitter.followers.each do |follower|
           if friends.include?(follower) == false
@@ -78,12 +79,35 @@ module C21
         end
 
         def parse_line(line)
+          result = []
           line.split("\t").each {|serif|
-            # Regex で対応できるはず
-            serif.gsub!(/.*「/, '').gsub!(/」/, '')
+            matched = serif.match(/^.*『(.*)』/)
+            result.push(matched[1])
           }
+
+          result
         end
       end
+
+      class Config
+        def initialize(dir='config')
+          @config = {}
+          @config = YAML.load_file(dir + '/bot.yaml')
+        end
+
+        def dog
+          @config['dog']
+        end
+
+        def man
+          @config['man']
+        end
+
+        def app
+          @config['app']
+        end
+      end
+
     end
   end
 
@@ -94,11 +118,48 @@ module C21
   class Man
     include C21::Twitter::Bot
   end
+
+  class Controller
+    def initialize()
+      self.setup_config()
+      self.setup_man(@config.man['app_conf'], @config.man['user_conf'], @config.app['channel'])
+      self.setup_dog(@config.dog['app_conf'], @config.dog['user_conf'], @config.app['channel'])
+      self.setup_serif(@config.app['serif_file'])
+
+      @logger  = Logger.new(STDOUT)
+    end
+
+    def setup_config
+      @config = C21::Twitter::Bot::Config.new()
+    end
+
+    def setup_man (app_conf, user_conf, channel)
+      @man = C21::Man.new(app_conf, user_conf, channel)
+    end
+
+    def setup_dog (app_conf, user_conf, channel)
+      @dog = C21::Dog.new(app_conf, user_conf, channel)
+    end
+
+    def setup_serif (serif_file)
+      @serif = C21::Twitter::Bot::Serif.new(serif_file)
+    end
+
+    def run()
+      serif_line = @serif.select_line
+      current_serif = @serif.parse_line(serif_line)
+
+      begin
+        @man.update(current_serif[0])
+      rescue
+        @logger.fatal('failed to update by man')
+      end
+
+      @dog.update(current_serif[1])
+
+      @man.auto_follow
+      @dog.auto_follow
+    end
+  end
 end
 
-# dog = C21::Dog.new('aaa')
-# man = C21::Man.new('bbb')
-
-serif = C21::Twitter::Bot::Serif.new('hoge')
-line = serif.select_line
-p serif.parse_line(line)
